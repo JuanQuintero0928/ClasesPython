cbe82495d3cd09b521e3a3c70e9a7c3d707d0bcf
Juan Jose <jjquintero0928@gmail.com>
2022-07-28 22:56:30 -0500

    se agrega try y except y raise al archivo debuggin

--- debuggin.py
+++ debuggin.py
@@ -1,14 +1,28 @@
+from pickle import TRUE
+
+
 def divisor(num):
-    divisor = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisor.append(i)
-    return divisor
+    try:
+        if num < 0:
+            raise ValueError("Debe ingresar un numero mayor a cero.")
+        divisor = []
+        for i in range(1, num + 1):
+            if num % i == 0:
+                divisor.append(i)
+        return divisor
+    except ValueError as ve:
+        print(ve)
+        return False
 
 def run():
-    num = int(input("Ingresa un numero : "))
-    print(divisor(num))
-    print("termino el programa")
+    try:
+        num = int(input("Ingresa un numero : "))
+        num = divisor(num)
+        if num != False:
+            print(num)
+            print("termino el programa")
+    except ValueError:
+        print("Debes Ingresar un numero")
 
 if __name__=='__main__':
     run()
